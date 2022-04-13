b1e586abf5239386fe46afeae8fd95f011954477
relaxyg <relaxyg4@gmail.com>
2022-04-13 14:58:57 +0600

    multi-search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,33 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    # if params[:query].present?
+      # @movies = Movie.where(title: params[:query])
+    #   sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+    #   @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+    # else
+    #   @movies = Movie.all
+    # end
+    # if params[:query].present?
+    #   sql_query = " \
+    #     movies.title ILIKE :query \
+    #     OR movies.synopsis ILIKE :query \
+    #     OR directors.first_name ILIKE :query \
+    #     OR directors.last_name ILIKE :query \
+    #   "
+    #   @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    # else
+    #   @movies = Movie.all
+    # end
+    if params[:query].present?
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.synopsis @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
